8efef1cd89f57be2cdf036a1ea13299bd0e96d7e
ian <hope030327@naver.com>
2025-01-21 03:23:03 +0900

    Docs: 이벤트 세부사항 조회 부분 주석 수정

--- controllers/events/crud.events.controller.js
+++ controllers/events/crud.events.controller.js
@@ -1,6 +1,9 @@
 import * as detailService from "../../services/events/crud.events.service.js";
 import { logError } from "../../utils/handlers/error.logger.js";
 
+/**
+ * events 테이블 중 주어진 eventId에 해당하는 튜플의 정보를 반환
+ */
 export const detailEvent = async(req, res, next) => {
   try {
     const { eventId } = req.params;
@@ -9,7 +12,7 @@ export const detailEvent = async(req, res, next) => {
 
     if (detail) {
       // 200
-      return res.status(200).success({ message: "이벤트 상세 정보 조회 성공" });
+      return res.status(200).success({ detail });
     }
     else if (!detail) {
       // 204
--- services/events/crud.events.service.js
+++ services/events/crud.events.service.js
@@ -1,11 +1,14 @@
 import db from "../../models/index.js";
 
 export const detailEvent = async (eventId) => {
+  // eventId가 유효하지 않은 경우 400
+  /**
+   * 아직 Ajv로 유효성 검사 안했어요
+   */
+
   const detail = await db.Events.findOne({
     where: { eventId: eventId }
   });
 
   return detail;
 };
-
-// eventId가 유효하지 않은 경우 400
