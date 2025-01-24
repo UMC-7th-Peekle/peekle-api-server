fcdc20f2a187076ddf2d50f24260d09d989ce3e8
ian <hope030327@naver.com>
2025-01-24 16:40:02 +0900

    Chore: PR 기반 코드 수정

--- services/events/create.events.service.js
+++ services/events/create.events.service.js
@@ -1,5 +1,5 @@
 import models from "../../models/index.js";
-
+import { InvalidInputError } from "../../utils/errors/errors.js";
 // 새로운 이벤트 생성
 export const newEvent = async (userId, eventData) => {
   const {
@@ -17,7 +17,7 @@ export const newEvent = async (userId, eventData) => {
 
   // 게시글 제목, 게시글 내용 누락 400
   if (!title || !content) {
-    throw new Error("게시글 제목 또는 내용이 누락되었습니다.");
+    throw new InvalidInputError("게시글 제목 또는 내용이 누락되었습니다.");
   }
   /**
    * 아직 Ajv로 유효성 검사 안했어요
