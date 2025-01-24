06e6124cc3cd5d5639295c6f365567cd6278ef0f
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-01-25 01:07:37 +0900

    Feat: 로그를 JSON 형태로 수정

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,9 +9,9 @@ module.exports = {
       // ignore_watch: ["node_modules", "logs", "public", "uploads"], // watch 대상에서 제외할 경로
       // max_memory_restart: "2G", // 메모리 사용량이 2GB를 넘으면 재시작
 
-      log_file: "./logs/pm2.combined.log",
-      error_file: "./logs/pm2.error.log",
-      out_file: "./logs/pm2.output.log",
+      log_file: "./logs/pm2/combined.log",
+      error_file: "./logs/pm2/error.log",
+      out_file: "./logs/pm2/output.log",
     },
   ],
 };
