e4aeda1c4ca4262f8cda1080dfd7397fc4f257b1
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-01-24 22:31:30 +0900

    Chore: 불용 코드 제거

--- utils/validators/auth/auth.validators.js
+++ utils/validators/auth/auth.validators.js
@@ -1,9 +1,3 @@
-import Ajv from "ajv";
-import addFormats from "ajv-formats";
-
-const ajv = new Ajv({ allErrors: true });
-addFormats(ajv);
-
 export const localRegisterSchema = {
   type: "object",
   properties: {
