906b6266f4b41f595b1550549a7500cbe105ff0c
ian <hope030327@naver.com>
2025-01-24 16:10:10 +0900

    Refactor: 이벤트 생성 부분 수정

--- services/events/create.events.service.js
+++ services/events/create.events.service.js
@@ -1,5 +1,4 @@
 import models from "../../models/index.js";
-import { Sequelize } from "sequelize";
 
 // 새로운 이벤트 생성
 export const newEvent = async (userId, eventData) => {
@@ -20,8 +19,10 @@ export const newEvent = async (userId, eventData) => {
   if (!title || !content) {
     throw new Error("게시글 제목 또는 내용이 누락되었습니다.");
   }
+  /**
+   * 아직 Ajv로 유효성 검사 안했어요
+   */
 
-  // schedules랑 image는 조인해서 가져와야 합니다.
   try {
     // 이벤트 생성
     const event = await models.Events.create({
@@ -39,13 +40,13 @@ export const newEvent = async (userId, eventData) => {
     // 해당 이벤트 스케줄 부분 튜플 생성
     const eventSchedules = schedules.map((schedule) => ({
       ...schedule,
-      eventId: event.eventId,
+      eventId: event.eventId
     }));
 
     await models.EventSchedules.bulkCreate(eventSchedules);
 
     return event;
   } catch (error) {
-    throw new Sequelize.DatabaseError(error);
+    throw error;
   }
 };
