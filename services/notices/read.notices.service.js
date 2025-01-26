773f567834c4c39bd517b0375383553fe418dd27
jam0928 <jack020928@naver.com>
2025-01-26 20:34:13 +0900

    Feat: 특정 공지 상세 조회에 이미지 정보 추가
    - 특정 공지사항 세부 내용 조회하는 것에 공지사항 이미지 정보 받아오는 부분 추가하였습니다.

--- services/notices/read.notices.service.js
+++ services/notices/read.notices.service.js
@@ -3,32 +3,47 @@ import { NotExistsError } from "../../utils/errors/errors.js";
 import models from "../../models/index.js";
 import logger from "../../utils/logger/logger.js";
 import { Op } from "sequelize";
+import { addBaseUrl } from "../../utils/upload/uploader.object.js";
 
 /**
  * 카테고리별 공지사항 조회
  */
-export const getNoticesByCategory = async ({ categoryId, limit, cursor }) => {
-};
+export const getNoticesByCategory = async ({ categoryId, limit, cursor }) => {};
 
 /**
  * 카테고리와 검색어로 공지사항 검색
  */
-export const searchNotices = async ({ category, query, limit, cursor }) => {
-};
+export const searchNotices = async ({ category, query, limit, cursor }) => {};
 
 /**
  * 공지사항 세부 내용 조회
  */
 export const getNoticesById = async ({ noticeId }) => {
   // 공지사항 조회
-  const notice = await models.Notices.findOne({
+  const data = await models.Notices.findOne({
     where: { noticeId },
+    include: [
+      {
+        model: models.NoticeImages,
+        as: "noticeImages",
+        attributes: ["imageUrl", "sequence"], // 필요한 필드만 가져오기
+      },
+    ],
   });
 
   // 공지사항이 존재하지 않는 경우
-  if (!notice) {
+  if (!data) {
     throw new NotExistsError("해당 공지사항이 존재하지 않습니다.");
   }
 
-  return { notice };
+  // 이미지 URL에 STATIC_FILE_BASE_URL 추가
+  const transformedImages = data.noticeImages.map((image) => ({
+    imageUrl: addBaseUrl(image.imageUrl),
+    sequence: image.sequence,
+  }));
+
+  return {
+    ...data.dataValues, // 공지사항 데이터
+    noticeImages: transformedImages, // 이미지 데이터
+  };
 };
