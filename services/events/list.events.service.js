6d7c19d25d6f68eaea33b21719307fde6f1db93f
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-01-26 14:20:35 +0900

    Chore: DB 수정사항 반영 및 주석처리

--- services/events/list.events.service.js
+++ services/events/list.events.service.js
@@ -1,5 +1,6 @@
 import models from "../../models/index.js";
 import { Op } from "sequelize";
+import { InvalidQueryError } from "../../utils/errors/errors.js";
 
 // 이벤트 목록 조회
 export const listEvent = async (category = "all", paginationOptions) => {
@@ -68,4 +69,43 @@ export const listEvent = async (category = "all", paginationOptions) => {
   const nextCursor = hasNextPage ? events[events.length - 1].eventId : null;
 
   return { events, nextCursor, hasNextPage };
+};
+
+export const validateQuery = (query) => {
+  const { limit, cursor, category, location, price, startDate, endDate } =
+    query;
+  // limit 및 cursor 유효성 검증 (정수형 확인)
+  const isInteger = (value) => /^\d+$/.test(value); // 정수만 허용
+  if ((limit && !isInteger(limit)) || (cursor && !isInteger(cursor))) {
+    throw new InvalidQueryError("limit, cursor는 정수여야 합니다.");
+  }
+
+  // 카테고리 검증
+  const categoryPool = ["all", "education", "culture", "activity"];
+  if (category && !categoryPool.includes(category)) {
+    throw new InvalidQueryError(
+      "올바르지 않은 카테고리입니다. 허용되는 값은 다음과 같습니다.",
+      categoryPool
+    );
+  }
+
+  // 날짜 형식 및 유효성 검증
+  // regex로 형식 검증 후 Date 객체로 변환하여 유효성 검증
+  const isValidDate = (dateString) => {
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!regex.test(dateString)) return false;
+    const date = new Date(dateString);
+    return date instanceof Date && !isNaN(date);
+  };
+
+  if (
+    (startDate && !isValidDate(startDate)) ||
+    (endDate && !isValidDate(endDate))
+  ) {
+    throw new InvalidQueryError("올바르지 않은 날짜 형식입니다.");
+  }
+
+  if (startDate && endDate && new Date(startDate) > new Date(endDate)) {
+    throw new InvalidQueryError("startDate가 endDate보다 미래일 수 없습니다.");
+  }
 };
