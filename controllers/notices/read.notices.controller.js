e26b31f3bee89f4f2b5bb82dc6643a2eed42e687
jam0928 <jack020928@naver.com>
2025-01-27 23:23:14 +0900

    Refactor: 카테고리별 공지 목록 조회 페이지네이션 방식 변경
    - cursor based에서 offset based로 변경

--- controllers/notices/read.notices.controller.js
+++ controllers/notices/read.notices.controller.js
@@ -7,15 +7,15 @@ import logger from "../../utils/logger/logger.js";
 export const getNoticesByCategory = async (req, res, next) => {
   try {
     const { categoryId } = req.params; // URL에서 categoryId 추출
-    const { limit, cursor } = req.query; // 쿼리 파라미터에서 limit와 cursor 추출
+    const { limit, offset } = req.query; // 쿼리 파라미터에서 limit와 cursor 추출
 
     // 페이지네이션 기본값 설정
     const paginationOptions = {
       limit: limit ? parseInt(limit, 10) : 10, // 기본 limit은 10
-      cursor: cursor || null, // cursor가 없으면 null
+      offset: offset ? parseInt(offset, 10) : 0, // 기본 limit은 10
     };
 
-    const { notices, nextCursor, hasNextPage } =
+    const { notices, totalCount } =
       await readNoticesService.getNoticesByCategory(
         categoryId,
         paginationOptions
@@ -25,6 +25,10 @@ export const getNoticesByCategory = async (req, res, next) => {
     if (notices && notices.length === 0) {
       return res.status(204).success(); // 응답 본문 없이 204 반환
     }
+
+    // 페이지 계산
+    const currentPage = Math.floor(paginationOptions.offset / paginationOptions.limit) + 1;
+    const totalPages = Math.ceil(totalCount / paginationOptions.limit);
 
     logger.debug("공지사항 목록 조회 성공", {
       action: "notices:getNoticesByCategory",
@@ -36,8 +40,9 @@ export const getNoticesByCategory = async (req, res, next) => {
     return res.status(200).success({
       message: "카테고리별 공지사항 조회 성공",
       notices,
-      nextCursor,
-      hasNextPage,
+      totalCount,
+      currentPage,
+      totalPages,
     });
   } catch (error) {
     logError(error);
@@ -49,15 +54,15 @@ export const getNoticesByCategory = async (req, res, next) => {
 export const searchNotices = async (req, res, next) => {
   try {
     const { category, query } = req.query; // Query에서 category, query, limit, cursor 추출
-    const { limit, cursor } = req.query; // 쿼리 파라미터에서 limit와 cursor 추출
+    const { limit, offset } = req.query; // 쿼리 파라미터에서 limit와 cursor 추출
 
     // 페이지네이션 기본값 설정
     const paginationOptions = {
       limit: limit ? parseInt(limit, 10) : 10, // 기본 limit은 10
-      cursor: cursor || null, // cursor가 없으면 null
+      offset: offset ? parseInt(offset, 10) : 0, // 기본 offset은 0
     };
 
-    const { notices } = await readNoticesService.searchNotices(
+    const { notices, totalCount } = await readNoticesService.searchNotices(
       category,
       query,
       paginationOptions
@@ -67,6 +72,10 @@ export const searchNotices = async (req, res, next) => {
     if (notices && notices.length === 0) {
       return res.status(204).success(); // 응답 본문 없이 204 반환
     }
+
+    // 페이지 계산
+    const currentPage = Math.floor(paginationOptions.offset / paginationOptions.limit) + 1;
+    const totalPages = Math.ceil(totalCount / paginationOptions.limit);
 
     logger.debug("공지사항 검색 성공", {
       action: "notices:searchNotices",
@@ -79,6 +88,9 @@ export const searchNotices = async (req, res, next) => {
     return res.status(200).success({
       message: "공지사항 검색 성공",
       notices,
+      totalCount,
+      currentPage,
+      totalPages,
     });
   } catch (error) {
     logError(error);
--- services/notices/read.notices.service.js
+++ services/notices/read.notices.service.js
@@ -1,5 +1,8 @@
 // Description: 공지사항 조회와 관련된 서비스 파일입니다.
-import { NotExistsError, InvalidQueryError } from "../../utils/errors/errors.js";
+import {
+  NotExistsError,
+  InvalidQueryError,
+} from "../../utils/errors/errors.js";
 import models from "../../models/index.js";
 import logger from "../../utils/logger/logger.js";
 import { Op } from "sequelize";
@@ -8,32 +11,18 @@ import { addBaseUrl } from "../../utils/upload/uploader.object.js";
 /**
  * 카테고리별 공지사항 조회
  */
-export const getNoticesByCategory = async (
-  categoryId,
-  { limit, cursor = null }
-) => {
-  // 공지사항 조회
-  const notices = await models.Notices.findAll({
-    where: {
-      categoryId,
-      ...(cursor && { noticeId: { [Op.lt]: cursor } }), // 커서 조건: noticeId 기준
-    },
+export const getNoticesByCategory = async (categoryId, paginationOptions) => {
+  console.log(categoryId, paginationOptions);
+  const result = await models.Notices.findAndCountAll({
+    where: { categoryId },
     order: [["createdAt", "DESC"]], // 최신순 정렬
-    limit: limit + 1,
-  });
-
-  // 다음 커서 설정
-  const hasNextPage = notices.length > limit; // limit + 1개를 가져왔으면 다음 페이지 있음
-  const nextCursor = hasNextPage ? notices[limit - 1].noticeId : null;
-
-  if (hasNextPage) {
-    notices.pop(); // limit + 1개를 가져왔으면 마지막 요소는 버림
-  }
+    limit: paginationOptions.limit, // 조회 개수 제한
+    offset: paginationOptions.offset, // 페이지네이션 옵션
+  })
 
   return {
-    notices,
-    nextCursor,
-    hasNextPage,
+    notices:result.rows,
+    totalCount: result.count,
   };
 };
 
@@ -43,7 +32,7 @@ export const getNoticesByCategory = async (
 export const searchNotices = async (
   category,
   query,
-  { limit, cursor = null }
+  { limit, offset }
 ) => {
   // 검색어가 없는 경우
   if (!category.trim() || !query.trim()) {
@@ -53,7 +42,6 @@ export const searchNotices = async (
     });
     throw new InvalidQueryError("검색어가 누락되었습니다.");
   }
-
 
   const notices = await models.NoticeCategory.findOne({
     where: { name: category },
