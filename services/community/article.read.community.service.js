fd99e57ace82e80352db7234f0780fbceea2e9fa
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-01-28 16:00:08 +0900

    Feat: 게시글 조회 시 좋아요, 댓글 개수 및 썸네일까지 응답하도록 수정

--- services/community/article.read.community.service.js
+++ services/community/article.read.community.service.js
@@ -3,6 +3,7 @@ import { NotExistsError } from "../../utils/errors/errors.js";
 import models from "../../models/index.js";
 import logger from "../../utils/logger/logger.js";
 import { Op } from "sequelize";
+import { addBaseUrl } from "../../utils/upload/uploader.object.js";
 
 /**
  * communityId에 해당하는 게시판의 게시글들을 가져옵니다
@@ -21,22 +22,67 @@ export const getArticles = async (communityId, { limit, cursor = null }) => {
         order: [["createdAt", "DESC"]], // 최신순 정렬
         limit: limit + 1, // 조회 개수 제한
         required: false, // 게시글이 없는 경우에도 커뮤니티는 반환
+        include: [
+          {
+            model: models.ArticleComments,
+            as: "articleComments",
+            attributes: ["commentId"], // 댓글 개수만 가져오기 위해 commentId만 추출
+            where: { status: { [Op.ne]: "deleted" } }, // 삭제되지 않은 댓글만 가져오기
+            required: false, // 댓글이 없는 경우에도 커뮤니티는 반환
+          },
+          {
+            model: models.ArticleLikes,
+            as: "articleLikes",
+            attributes: ["likedUserId"], // 좋아요 개수만 가져오기 위해 userId만 추출
+            required: false, // 좋아요가 없는 경우에도 커뮤니티는 반환
+          },
+          {
+            model: models.ArticleImages,
+            as: "articleImages",
+            attributes: ["imageUrl", "sequence"], // 필요한 필드만 가져오기
+            where: { sequence: 1 }, // 대표 이미지만 가져오기
+            limit: 1,
+            required: false, // 이미지가 없는 경우에도 커뮤니티는 반환
+          },
+        ],
       },
     ],
   });
 
   if (!community) {
     // 게시판이 존재하지 않는 경우
-    logger.error(`[getArticles] 존재하지 않는 communityId - ${communityId}`);
+    logger.error("존재하지 않은 게시판입니다.", {
+      action: "community:getArticles",
+      actionType: "error",
+      communityId: communityId,
+    });
     throw new NotExistsError("해당 게시판이 존재하지 않습니다.");
   }
 
   // 게시글만 추출
   const articles = community.articles;
+  articles.map((article) => {
+    article = article.dataValues;
+    article.articleComments = article.articleComments.length; // 댓글 개수만 추출
+    article.articleLikes = article.articleLikes.length; // 좋아요 개수만 추출
+    if (article.articleImages.length > 0) {
+      article.articleImages = addBaseUrl(article.articleImages[0].imageUrl); // 대표 이미지 URL
+    } else {
+      article.articleImages = null;
+    }
+    article.thumbnail = article.articleImages;
+    delete article.articleImages;
+  });
 
-  logger.debug(
-    `[getArticles] 조회된 데이터 개수 - 총 ${articles.length}개, 요청 limit: ${limit}`
-  );
+  logger.debug("게시글 조회 완료", {
+    action: "community:getArticles",
+    actionType: "success",
+    data: {
+      communityId: communityId,
+      articleCount: articles.length,
+      limit: limit,
+    },
+  });
 
   // 다음 커서 설정
   const hasNextPage = articles.length > limit; // limit + 1개를 가져왔으면 다음 페이지 있음
