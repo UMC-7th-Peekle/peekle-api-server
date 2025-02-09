e003fabe29cf92894331d0d21c733e2801d12007
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-02-09 18:37:13 +0900

    Feat: HRBAC permission checker

--- middleware/check.permission.js
+++ middleware/check.permission.js
@@ -1,104 +1,128 @@
+import { Op } from "sequelize";
 import models from "../models/index.js";
 import {
-  InvalidInputError,
   LackPermissionError,
   UnauthorizedError,
 } from "../utils/errors/errors.js";
 import logger from "../utils/logger/logger.js";
 
 export const checkPermission = (permission) => {
-  if (permission == "admin") {
-    // 현재는 admin:super가 최고관리자 권한입니다.
-    // 모든 권한을 전부 가지고 있는 슈퍼권한이라고 생각하시면 됩니다.
-    // 나머지 권한에 대한 세분화는 추후에 진행할 예정입니다.
+  return async (req, res, next) => {
+    if (!req?.user?.userId) {
+      logger.debug("인증 정보가 제공되지 않았습니다.");
+      return next(new UnauthorizedError("인증 정보가 제공되지 않았습니다."));
+    }
 
-    return async (req, res, next) => {
-      if (!req?.user?.userId) {
-        logger.debug("인증 정보가 제공되지 않았습니다.");
-        return next(new UnauthorizedError("인증 정보가 제공되지 않았습니다."));
-      }
+    const result = await checkUserPermissions({
+      userId: req.user.userId,
+      permission: permission,
+    });
 
-      const result = await checkUserPermissions({
-        userId: req.user.userId,
-        permission: "admin:super",
-      });
+    if (!result) {
+      logger.debug("권한이 없습니다.");
+      return next(new LackPermissionError("권한이 없습니다."));
+    }
 
-      if (!result) {
-        logger.debug("권한이 없습니다.");
-        return next(new LackPermissionError("권한이 없습니다."));
-      }
-
-      return next();
-    };
-  } else if ((permission = "crudArticle")) {
-    return async (req, res, next) => {
-      next();
-    };
-  } else if ((permission = "crudEvent")) {
-    return async (req, res, next) => {
-      next();
-    };
-  } else {
-    return async (req, res, next) => {
-      return next(new InvalidInputError("잘못된 권한 확인 요청입니다."));
-    };
-  }
+    return next();
+  };
 };
 
 const checkUserPermissions = async ({ userId, permission }) => {
-  // 1. user가 가진 role들을 찾고, 그 parent role까지 찾는다.
-  // 2. role + parent role까지 포함했을 떄, 해당 role들이
-  // 가지고 있는 permission들 중에 찾고자 하는
-  // permission이 있는지 비교한다.
-
   const userRoles = await models.UserRoles.findAll({
-    where: { userId: userId },
-    include: [
-      {
-        model: models.Roles,
-        as: "role",
-        required: true,
-        include: [
-          // 1번째 include, parent role의 permission을 찾기
-          {
-            model: models.Roles,
-            as: "parentRole",
-            required: false,
-            include: [
-              {
-                model: models.Permissions,
-                as: "permissionIdPermissions",
-                through: { model: models.RolePermissions },
-                where: { name: permission },
-                required: true,
-              },
-            ],
-          },
-          // 2번째 include, role의 permission을 찾기
-          {
-            model: models.Permissions,
-            as: "permissionIdPermissions",
-            through: { model: models.RolePermissions },
-            where: { name: permission },
-            required: false,
-          },
-        ],
-      },
-    ],
+    where: {
+      userId: userId,
+    },
   });
 
-  // console.log(userRoles[0]);
-  // console.log("\n\n\n\n\n\n");
-  // console.log(userRoles[0].role.parentRole);
-  // console.log("\n\n\n\n\n\n");
-  // console.log(userRoles[0].role.permissionIdPermissions);
-  // console.log("\n\n\n\n\n\n");
-  // console.log(
-  //   userRoles[0].role.parentRole ||
-  //     userRoles[0].role.permissionIdPermissions.length > 0
-  // );
-  return (
-    userRoles[0].role.parentRole ||
-    userRoles[0].role.permissionIdPermissions.length > 0
-  );
+  const recursivelyFindPermissions = async ({ roleId, permission }) => {
+    const roles = await models.Roles.findByPk(roleId, {
+      include: [
+        // 해당 사용자가 가지고 있는 role이 가지고 있는 permission을 찾음
+        {
+          model: models.Permissions,
+          as: "permissionIdPermissions",
+          through: { model: models.RolePermissions },
+          where: { name: permission },
+          required: false,
+        },
+        // 해당 사용자의 parent role을 찾고 끝낸다.
+        {
+          model: models.Roles,
+          through: { model: models.RoleHierarchy },
+          as: "parentRoleIdRoles", // 본인의 부모 역할을 찾습니다
+          required: false,
+        },
+      ],
+    });
+
+    // console.log(roles.dataValues);
+
+    if (roles.permissionIdPermissions.length > 0) {
+      // console.log("IF 1 :", roles.permissionIdPermissions);
+      return true;
+    } else {
+      for (const parentRole of roles.parentRoleIdRoles) {
+        const found = await recursivelyFindPermissions({
+          roleId: parentRole.roleId,
+          permission,
+        });
+        if (found) return true;
+      }
+    }
+  };
+
+  for (const role of userRoles) {
+    const result = await recursivelyFindPermissions({
+      roleId: role.roleId,
+      permission,
+    });
+    if (result) return true;
+  }
+
+  return false;
 };
+
+/*
+    1. UserRoles에서 사용자의 역할들을 모두 찾는다
+    2. 해당 role의 permission과, RoleHierarchy를 통해 알 수 있는 parent role의 permission을 살펴본다
+    3. 존재하면 true 아니면 fasle
+    4. 이 코드 그대로 재귀로 조져야 함 원래
+    5. 하지만 귀찮고 에러가 너무 짜증나져서 우선 1 depth만 허용하는 것으로 함.
+    6. 우리 서비스에서 2 depth 사용하는 것은 현재 community -> article -> comment 임
+    
+    parent Role이 가지고 있는 권한은 child Role 또한 가지고 있음.
+    그렇다면 우리는 나의 parent role이 가지고 있는 
+
+    1. 해당 사용자가 가지고 있는 role들을 살펴본다. 해당 role이 내가 찾는 permission을 가지고 있는지 본다.
+    2. 없다면, 해당 사용자가 가지고 있는 role들의 parent를 본다. 해당 parent가 내가 찾는 permission을 가지고 있는지 본다.
+    .
+  */
+
+// TODO : 단일 레벨의 부모만을 고려함
+// const userRoles = await models.UserRoles.findOne({
+//   where: { userId: userId },
+//   include: [
+//     {
+//       model: models.Roles,
+//       as: "role",
+//       required: true,
+//       include: [
+//         // 해당 사용자가 가지고 있는 role이 가지고 있는 permission을 찾음
+//         {
+//           model: models.Permissions,
+//           as: "permissionIdPermissions",
+//           through: { model: models.RolePermissions },
+//           where: { name: permission },
+//           required: false,
+//         },
+//         // 해당 사용자의 parent role을 찾고 끝낸다.
+//         {
+//           model: models.Roles,
+//           through: { model: models.RoleHierarchy },
+//           as: "parentRoleIdRoles", // 본인의 부모 역할을 찾습니다
+//           required: false,
+//         },
+//       ],
+//     },
+//   ],
+// });
--- models/seed/seeder.js
+++ models/seed/seeder.js
@@ -428,123 +428,6 @@ export const seedUsers = async () => {
 };
 
 export const seedPermissions = async () => {
-  const crudFields = ["community", "article", "comment", "events", "tickets"];
-
-  const curdForm = (field) => {
-    return [
-      {
-        name: `${field}:create`,
-        description: `${field} 생성 권한`,
-      },
-      {
-        name: `${field}:read`,
-        description: `${field} 조회 권한`,
-      },
-      {
-        name: `${field}:update`,
-        description: `${field} 수정 권한`,
-      },
-      {
-        name: `${field}:delete`,
-        description: `${field} 삭제 권한`,
-      },
-    ];
-  };
-
-  let permissions = [];
-  crudFields.forEach((field) => {
-    permissions = [...permissions, ...curdForm(field)];
-  });
-  permissions.push({
-    name: "admin:super",
-    description: "SUPER ADMIN",
-  });
-
-  const customPerms = [
-    {
-      name: "user:ban",
-      description: "사용자 차단 권한",
-    },
-    {
-      name: "user:permanent_ban",
-      description: "사용자 영구 차단 권한",
-    },
-    {
-      name: "user:unban",
-      description: "사용자 차단 해제 권한",
-    },
-  ];
-  permissions = [...permissions, ...customPerms];
-
-  const roles = [
-    {
-      name: "super_admin",
-      description: "최고 관리자 권한",
-    },
-    {
-      name: "crudCommunity",
-      description: "커뮤니티 CRUD 권한",
-    },
-    {
-      name: "crudArticle",
-      description: "게시글 CRUD 권한",
-    },
-    {
-      name: "crudComment",
-      description: "댓글 CRUD 권한",
-    },
-    {
-      name: "crudEvent",
-      description: "이벤트 CRUD 권한",
-    },
-    {
-      name: "crudTicket",
-      description: "티켓 CRUD 권한",
-    },
-  ];
-
-  let rolePermissions = [];
-
-  // role: super_admin 에게 모든 권한 부여
-  for (let i = 1; i <= permissions.length; i++) {
-    rolePermissions.push({
-      roleId: roles.findIndex((role) => role.name === "super_admin") + 1,
-      permissionId: i,
-    });
-  }
-
-  // crud 카테고리에 해당하는 각 role 생성 및 권한 부여
-  // 권한은 index를 찾아서 부여하는 방식임
-  let idx = 2;
-  for (const category of crudFields) {
-    rolePermissions.push(
-      {
-        roleId: idx,
-        permissionId:
-          permissions.findIndex((perm) => perm.name === `${category}:create`) +
-          1,
-      },
-      {
-        roleId: idx,
-        permissionId:
-          permissions.findIndex((perm) => perm.name === `${category}:read`) + 1,
-      },
-      {
-        roleId: idx,
-        permissionId:
-          permissions.findIndex((perm) => perm.name === `${category}:update`) +
-          1,
-      },
-      {
-        roleId: idx,
-        permissionId:
-          permissions.findIndex((perm) => perm.name === `${category}:delete`) +
-          1,
-      }
-    );
-    idx += 1;
-  }
-
   await models.UserRoles.destroy({
     where: {},
   });
@@ -565,8 +448,67 @@ export const seedPermissions = async () => {
   await models.sequelize.query("TRUNCATE TABLE user_roles;");
   await models.sequelize.query("SET foreign_key_checks = 1;");
 
+  const permissions = [
+    { permissionId: 1, name: "admin:super", description: "최고 관리자 권한" },
+    { permissionId: 2, name: "admin:user", description: "사용자 관리 권한" },
+    { permissionId: 3, name: "admin:ticket", description: "티켓 관리 권한" },
+    { permissionId: 4, name: "admin:event", description: "이벤트 관리 권한" },
+    {
+      permissionId: 5,
+      name: "admin:community",
+      description: "커뮤니티 관리 권한",
+    },
+    { permissionId: 6, name: "admin:article", description: "게시글 관리 권한" },
+    { permissionId: 7, name: "admin:comment", description: "댓글 관리 권한" },
+    { permissionId: 8, name: "ticket:admin", description: "티켓 관리 권한" },
+    { permissionId: 9, name: "user:admin", description: "사용자 관리 권한" },
+    { permissionId: 10, name: "event:admin", description: "이벤트 관리 권한" },
+    {
+      permissionId: 11,
+      name: "community:admin",
+      description: "커뮤니티 관리 권한",
+    },
+    {
+      permissionId: 12,
+      name: "article:admin",
+      description: "게시글 관리 권한",
+    },
+    { permissionId: 13, name: "comment:admin", description: "댓글 관리 권한" },
+  ];
+
+  const roles = [
+    { roleId: 1, name: "admin:super", description: "최고 관리자 역할" },
+    { roleId: 2, name: "admin:user", description: "사용자 관리자 역할" },
+    { roleId: 3, name: "admin:ticket", description: "티켓 관리자 역할" },
+    { roleId: 4, name: "admin:event", description: "이벤트 관리자 역할" },
+    { roleId: 5, name: "admin:community", description: "커뮤니티 관리자 역할" },
+    { roleId: 6, name: "admin:article", description: "게시글 관리자 역할" },
+    { roleId: 7, name: "admin:comment", description: "댓글 관리자 역할" },
+  ];
+
+  const rolePermissions = [
+    { roleId: 1, permissionId: 1 },
+    { roleId: 2, permissionId: 9 },
+    { roleId: 3, permissionId: 8 },
+    { roleId: 4, permissionId: 10 },
+    { roleId: 5, permissionId: 11 },
+    { roleId: 6, permissionId: 12 },
+    { roleId: 7, permissionId: 13 },
+  ];
+
+  const roleHierarchy = [
+    { parentRoleId: 2, childRoleId: 1 },
+    { parentRoleId: 3, childRoleId: 1 },
+    { parentRoleId: 4, childRoleId: 1 },
+    { parentRoleId: 5, childRoleId: 1 },
+    { parentRoleId: 6, childRoleId: 5 },
+    { parentRoleId: 7, childRoleId: 6 },
+  ];
+
+  // bulkCreate 예시
   await models.Permissions.bulkCreate(permissions, { logging: false });
   await models.Roles.bulkCreate(roles, { logging: false });
+  await models.RoleHierarchy.bulkCreate(roleHierarchy, { logging: false });
   await models.RolePermissions.bulkCreate(rolePermissions, { logging: false });
 
   logger.warn("Permissions에 대한 Seeding이 실행되었습니다.", {
