5726f6116c49aa8db1009c9e813e645c77891020
jam0928 <jack020928@naver.com>
2025-02-09 12:31:32 +0900

    Refactor: 게시글 및 댓글 신고 공통 로직 분리
    - 게시글 및 댓글  존재 여부 확인하는 checkTargetExists, 이미 신고한 게시글 혹은 댓글인지 확인하는 checkIfAlreadyReported, 신고를 작성하는 createReport가 공통으로 존재하는 로직이라 분리하였습니다.
    - controller에서 불필요하게 반환하던 report에 대한 내용은 제거하고 성공 메시지만 반환하도록 하였습니다.

--- services/community/report.community.service.js
+++ services/community/report.community.service.js
@@ -7,99 +7,95 @@ import {
 import models from "../../models/index.js";
 import logger from "../../utils/logger/logger.js";
 
-// 게시글 신고
-export const reportArticle = async ({
+/**
+ * 게시글이 존재하는지 확인합니다.
+ */
+const checkTargetExists = async (
+  targetType,
+  targetId,
   communityId,
-  articleId,
-  reportedUserId,
-  reason,
-}) => {
-  // 게시글 조회
-  const article = await models.Articles.findOne({
-    where: { communityId, articleId },
-  });
-
-  // 게시글이 존재하지 않는 경우: 404
-  if (!article) {
-    throw new NotExistsError("존재하지 않는 게시글입니다.");
+  articleId
+) => {
+  let target;
+  if (targetType === "article") {
+    target = await models.Articles.findOne({
+      where: { communityId, articleId: targetId }, // 게시글은 communityId와 articleId로 조회
+    });
+  } else if (targetType === "comment") {
+    target = await models.ArticleComments.findOne({
+      where: { articleId, commentId: targetId }, // 댓글은 articleId와 commentId로 조회
+    });
   }
 
-  // 신고자와 게시글 작성자가 같은 경우: 403
-  if (article.authorId === reportedUserId) {
-    throw new NotAllowedError("자신이 작성한 게시글은 신고할 수 없습니다.");
+  if (!target) {
+    throw new NotExistsError(`존재하지 않는 ${targetType}입니다.`);
   }
-
-  // 신고 내역 조회
-  const isReported = await models.Reports.findOne({
-    where: { targetId: articleId, reportedUserId, type: "article" },
-  });
-
-  // 이미 신고한 경우: 409
-  // 기존 방식 if (isReported)는 조건에서 targetId에 대한 검증이 없어서 테스트 코드 적용이 어려웠음
-  if (isReported.targetId === articleId) {
-    throw new AlreadyExistsError("이미 신고한 게시글입니다.");
-  }
-
-  // 신고 작성
-  const newReport = await models.Reports.create({
-    targetId: articleId,
-    reportedUserId,
-    reason,
-    type: "article",
-  });
-
-  return { newReport };
+  return target;
 };
 
-// 댓글 신고
-export const reportComment = async ({
+/**
+ * 이미 신고한 게시글인지 확인합니다.
+ */
+const checkIfAlreadyReported = async (targetId, reportedUserId, type) => {
+  const isReported = await models.Reports.findOne({
+    where: { targetId, reportedUserId, type },
+  });
+
+  if (isReported) {
+    throw new AlreadyExistsError(
+      `이미 신고한 ${type === "article" ? "게시글" : "댓글"}입니다.`
+    );
+  }
+};
+
+/**
+ * 게시글을 신고합니다.
+ */
+const createReport = async ({ targetId, reportedUserId, reason, type }) => {
+  return await models.Reports.create({
+    targetId,
+    reportedUserId,
+    reason,
+    type,
+  });
+};
+
+/**
+ * 게시글 및 댓글을 신고합니다.
+ */
+export const reportTarget = async ({
+  targetType,
   communityId,
   articleId,
-  commentId,
+  commentId = null,
   reportedUserId,
   reason,
 }) => {
-  // 댓글 조회
-  const comment = await models.ArticleComments.findOne({
-    where: { articleId, commentId },
-  });
+  const targetId = targetType === "article" ? articleId : commentId;
 
-  // 댓글이 존재하지 않는 경우: 404
-  if (!comment) {
-    logger.error(
-      `[reportComment] 댓글이 존재하지 않음 - articleId: ${articleId}, commentId: ${commentId}`
-    );
-    throw new NotExistsError("해당 댓글을 찾을 수 없습니다.");
+  await checkTargetExists(targetType, targetId, communityId, articleId);
+
+  if (targetType === "article") {
+    const article = await models.Articles.findOne({ where: { articleId } });
+    if (article.authorId === reportedUserId) {
+      throw new NotAllowedError("자신이 작성한 게시글은 신고할 수 없습니다.");
+    }
+  } else if (targetType === "comment") {
+    const comment = await models.ArticleComments.findOne({
+      where: { commentId },
+    });
+    if (comment.authorId === reportedUserId) {
+      throw new NotAllowedError("자신이 작성한 댓글은 신고할 수 없습니다.");
+    }
   }
 
-  // 신고자와 댓글 작성자가 같은 경우: 403
-  if (comment.authorId === reportedUserId) {
-    logger.error(
-      `[reportComment] 자신이 작성한 댓글은 신고할 수 없습니다 - articleId: ${articleId}, commentId: ${commentId}`
-    );
-    throw new NotAllowedError("자신이 작성한 댓글은 신고할 수 없습니다.");
-  }
+  await checkIfAlreadyReported(targetId, reportedUserId, targetType);
 
-  // 신고 내역 조회
-  const isReported = await models.Reports.findOne({
-    where: { targetId: commentId, reportedUserId, type: "comment" },
-  });
-
-  // 이미 신고한 경우: 409
-  // 기존 방식 if (isReported)는 조건에서 targetId에 대한 검증이 없어서 테스트 코드 적용이 어려웠음
-  if (isReported.targetId === commentId) {
-    logger.error(
-      `[reportComment] 이미 신고한 댓글입니다 - articleId: ${articleId}, commentId: ${commentId}`
-    );
-    throw new AlreadyExistsError("이미 신고한 댓글입니다.");
-  }
-
-  // 신고 작성
-  const newReport = await models.Reports.create({
-    targetId: commentId,
+  const newReport = await createReport({
+    targetId,
     reportedUserId,
     reason,
-    type: "comment",
+    type: targetType,
   });
 
   return { newReport };
